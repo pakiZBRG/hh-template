904026211b5c86990643f0e94abc9aca211b82e2
Nikola Pavlovic <pavlovicnikola511@gmail.com>
2022-07-31 08:59:16 +0200

    Adding Chainlink Keepers

---
+++ contracts/interfaces/Keeper.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+error Contract__UpkeepNotNeeded();
+
+contract Contract is KeeperCompatible {
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
+
+    constructor(uint256 interval) {
+        s_lastTimeStamp = block.timestamp;
+        i_interval = interval;
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory /* performData */
+        )
+    {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+        return upkeepNeeded;
+    }
+
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) revert Contract__UpkeepNotNeeded();
+        // Do something
+    }
+}
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -4,14 +4,15 @@ const networkConfig = {
     vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    subscriptionId: "9534", //vrf.chain.link
+    subscriptionId: "9534", // vrf.chain.link
     callbackGasLimit: "200000",
+    interval: "30"
   },
   31337: {
     name: "localhost",
-    entranceFee: ethers.utils.parseEther("0.01"),
     gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-    callbackGasLimit: "500000",
+    callbackGasLimit: "200000",
+    interval: "30"
   },
 };
 
